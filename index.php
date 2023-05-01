043d913c4f86761c5ad4b3f64acea08bf9d8a7f7
cliexec <cli.exec.com@gmail.com>
2023-05-01 11:25:09 +0700

    added home

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme and one of the
+ * two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * For example, it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+		<?php if ( have_posts() ) : ?>
+
+			<?php
+			// Start the loop.
+			while ( have_posts() ) :
+				the_post();
+				?>
+				<?php if ( is_single() ) : ?>
+					<?php get_template_part( 'content', get_post_format() ); ?>
+				<?php else : ?>
+					<?php get_template_part( 'content', 'item' ); ?>
+				<?php endif; ?>
+			<?php endwhile; ?>
+
+		<?php else : ?>
+			<?php get_template_part( 'content', 'none' ); ?>
+		<?php endif; ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
