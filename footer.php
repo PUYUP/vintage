f36709cbc3190e4ae96a2f27b510f40171204d61
cliexec <cli.exec.com@gmail.com>
2023-05-15 23:49:04 +0700

    italic cp

--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer mt-auto flex-none">
-		<div class="w-full xl:w-3/6 lg:w-4/6 md:w-4/5 mx-auto text-neutral-500 text-right text-sm">
+		<div class="w-full xl:w-3/6 lg:w-4/6 md:w-4/5 mx-auto text-neutral-500 text-right italic text-xs">
 			<div class="px-4 py-2 xl:px-12 xl:py-2 lg:px-8 lg:py-2 md:px-6 md:py-2">
 				<div class="copyright">
 					<?php
